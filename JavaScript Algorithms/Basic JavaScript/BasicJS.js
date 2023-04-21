a0a641521ef1f659f99de22c2c7f8653eb17156d
Marina Vyatkina <marinavyatk74@mail.ru>
2023-04-21 21:37:14 +0400

    Add tasks 31-35

--- JavaScript Algorithms/Basic JavaScript/BasicJS.js
+++ JavaScript Algorithms/Basic JavaScript/BasicJS.js
@@ -6,91 +6,91 @@ multiple
 line*/
 
 // // 2.Declare JavaScript Variables
-// var myName;
+var myName;
 
 // // 3.Storing Values with the Assignment Operator
-// var a;
-// a = 7;
+var a;
+a = 7;
 
 // 4. Assigning the Value of One Variable to Another
-// var a;
-// a = 7;
-// var b;
-// b = a;
+var a;
+a = 7;
+var b;
+b = a;
 
 // 5. Initializing Variables with the Assignment Operator
-// var myVar = 0;
-// var a = 9;
+var myVar = 0;
+var a = 9;
 
 // 6. Declare String Variables
-// var myFirstName = "Marina";
-// var myLastName = "Vyatkina";
+var myFirstName = "Marina";
+var myLastName = "Vyatkina";
 
 // 7.Understanding Uninitialized Variables
-// var a;
-// var b;
-// var c;
-// a = 5;
-// b = 10;
-// c = "I am a";
-// a = a + 1;
-// b = b + 5;
-// c = c + " String!";
+var a;
+var b;
+var c;
+a = 5;
+b = 10;
+c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
 
 // 8.Understanding Case Sensitivity in Variables
-// // Variable declarations
-// var studlyCapVar;
-// var properCamelCase;
-// var titleCaseOver;
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
 
-// // Variable assignments
-// studlyCapVar = 10;
-// properCamelCase = "A String";
-// titleCaseOver = 9000;
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
 
 // 9. Explore Differences Between the var and let Keywords
-// let catName = "Oliver";
-// let catSound = "Meow!";
+let catName = "Oliver";
+let catSound = "Meow!";
 
 // 10. Declare a Read-Only Variable with the const Keyword
-// const FCC = "freeCodeCamp"; // Change this line
-// let fact = "is cool!"; // Change this line
-// fact = "is awesome!";
-// console.log(FCC, fact); // Change this line
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
 
 // 11.Add Two Numbers with JavaScript
-// const sum = 10 + 10;
+const sum = 10 + 10;
 
-12. Subtract One Number from Another with JavaScript
+// 12. Subtract One Number from Another with JavaScript
 const difference = 45 - 33;
 
-13. Multiply Two Numbers with JavaScript
+// 13. Multiply Two Numbers with JavaScript
 const product = 8 * 10;
 
-14. Divide One Number by Another with JavaScript
+// 14. Divide One Number by Another with JavaScript
 const quotient = 66 / 33;
 
-15. Increment a Number with JavaScript
+// 15. Increment a Number with JavaScript
 let myVar = 87;
 myVar++;
 
-16. Decrement a Number with JavaScript
+// 16. Decrement a Number with JavaScript/
 let myVar = 11;
 myVar--;
 
-17. Create Decimal Numbers with JavaScript
+// 17. Create Decimal Numbers with JavaScript
 const myDecimal = 5.7;
 
-18. Multiply Two Decimals with JavaScript
+// 18. Multiply Two Decimals with JavaScript
 const product = 5.0 * 1.0;
 
-19. Divide One Decimal by Another with JavaScript
+// 19. Divide One Decimal by Another with JavaScript
 const quotient = 4.4 / 2.0;
 
-20. Finding a Remainder in JavaScript
+// 20. Finding a Remainder in JavaScript
 const remainder = 11%3;
 
-21. Compound Assignment With Augmented Addition
+// 21. Compound Assignment With Augmented Addition
 let a = 3;
 let b = 17;
 let c = 12;
@@ -98,7 +98,7 @@ a += 12;
 b += 9;
 c += 7;
 
-22. Compound Assignment With Augmented Subtraction
+// 22. Compound Assignment With Augmented Subtraction
 let a = 11;
 let b = 9;
 let c = 3;
@@ -106,7 +106,7 @@ a -=  6;
 b -=  15;
 c -=  1;
 
-23. Compound Assignment With Augmented Multiplication
+// 23. Compound Assignment With Augmented Multiplication
 let a = 5;
 let b = 12;
 let c = 4.6;
@@ -114,7 +114,7 @@ a *= 5;
 b *= 3;
 c *= 10;
 
-24. Compound Assignment With Augmented Division
+// 24. Compound Assignment With Augmented Division
 let a = 48;
 let b = 108;
 let c = 33;
@@ -122,22 +122,45 @@ a /= 12;
 b /= 4;
 c /= 11;
 
-25. Escaping Literal Quotes in Strings
+// 25. Escaping Literal Quotes in Strings
 const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 
-26. Quoting Strings with Single Quotes
+// 26. Quoting Strings with Single Quotes
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
-27. Escape Sequences in Strings
+// 27. Escape Sequences in Strings
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 
-28. Concatenating Strings with Plus Operator
+// 28. Concatenating Strings with Plus Operator
 const myStr = "This is the start. " + "This is the end.";
 
-29. Concatenating Strings with the Plus Equals Operator
+// 29. Concatenating Strings with the Plus Equals Operator
 let myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.
 
-30. Constructing Strings with Variables
+// 30. Constructing Strings with Variables
 const myName = "Marina";
 const myStr = "My name is " + myName + "and I am well!";
+
+// 31. Appending Variables to Strings
+const someAdjective = "fun";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// 32. Find the Length of a String
+let lastNameLength = 0;
+const lastName = "Lovelace";
+lastNameLength = lastName.length;
+
+// 33. Use Bracket Notation to Find the First Character in a String
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
+
+// 34. Understand String Immutability
+let myStr = "Jello World";
+myStr = "Hello World";
+
+// 35. Use Bracket Notation to Find the Nth Character in a String
+const lastName = "Lovelace";
+const thirdLetterOfLastName = lastName[2];
