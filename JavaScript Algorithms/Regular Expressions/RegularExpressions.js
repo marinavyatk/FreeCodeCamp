c279a723215494d47731e48a70f8ab2e6978c2e0
Marina Vyatkina <marinavyatk74@mail.ru>
2023-05-03 22:48:23 +0400

    Added tasks 26-33

--- JavaScript Algorithms/Regular Expressions/RegularExpressions.js
+++ JavaScript Algorithms/Regular Expressions/RegularExpressions.js
@@ -121,3 +121,44 @@ let result = sample.match(countNonWhiteSpace);
 let ohStr = "Ohhh no";
 let ohRegex = /oh{3,6} no/gi;
 let result = ohRegex.test(ohStr);
+
+// 26. Specify Only the Lower Number of Matches
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/;
+let result = haRegex.test(haStr);
+
+// 27. Specify Exact Number of Matches
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/;
+let result = timRegex.test(timStr);
+
+// 28. Check for All or None
+let favWord = "favorite";
+let favRegex = /favou?rite/;
+let result = favRegex.test(favWord);
+
+// 29. Positive and Negative Lookahead
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6,})(?=\D*\d{2})/;
+let result = pwRegex.test(sampleWord);
+
+// 30. Check For Mixed Grouping of Characters
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor)( \w.)* Roosevelt/g;
+let result = myRegex.test(myString);
+
+// 31. Reuse Patterns Using Capture Groups
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+) \1 \1$/g;
+let result = reRegex.test(repeatNum);
+
+// 32. Use Capture Groups to Search and Replace
+let str = "one two three";
+let fixRegex = /(\w+) (\w+) (\w+)/;
+let replaceText = "$3 $2 $1";
+let result = str.replace(fixRegex, replaceText);
+
+// 33. Remove Whitespace from Start and End
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g;
+let result = hello.replace(wsRegex, "");
