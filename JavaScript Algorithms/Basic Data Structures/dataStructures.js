8312ffc14aeb4d4cca558dfe01b4c8f431bc6813
Marina Vyatkina <marinavyatk74@mail.ru>
2023-05-06 01:31:25 +0400

    Added tasks 11-20

--- JavaScript Algorithms/Basic Data Structures/dataStructures.js
+++ JavaScript Algorithms/Basic Data Structures/dataStructures.js
@@ -79,3 +79,202 @@ function quickCheck(arr, elem) {
   return arr.indexOf(elem) < 0 ? false : true;
 }
 console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
+
+// 11. Iterate Through All an Array's Items Using For Loops
+function filteredArray(arr, elem) {
+  let newArr = [];
+  // Only change code below this line
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr[i].includes(elem)) {
+      newArr.push(arr[i]);
+    }
+  }
+  // Only change code above this line
+  return newArr;
+}
+
+console.log(
+  filteredArray(
+    [
+      [3, 2, 3],
+      [1, 6, 3],
+      [3, 13, 26],
+      [19, 3, 9],
+    ],
+    3
+  )
+);
+
+// 12. Create complex multi-dimensional arrays
+let myNestedArray = [
+  ["unshift", false, 1, 2, 3, "complex", "nested"],
+  ["loop", "shift", 6, 7, 1000, "method"],
+  ["concat", false, true, "spread", "array"],
+  ["mutate", 1327.98, "splice", "slice", "push"],
+  ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth"],
+];
+
+myNestedArray[4][1] = ["deep", ["deeper", ["deepest"]]];
+
+// 13. Add Key-Value Pairs to JavaScript Objects
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+};
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+console.log(foods);
+
+// 14. Modify an Object Nested Within an Object
+let userActivity = {
+  id: 23894201352,
+  date: "January 1, 2017",
+  data: {
+    totalUsers: 51,
+    online: 42,
+  },
+};
+userActivity.data.online = 45;
+console.log(userActivity);
+
+// 15. Access Property Names with Bracket Notation
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+function checkInventory(scannedItem) {
+  return foods[scannedItem];
+}
+
+console.log(checkInventory("apples"));
+
+// 16. Use the delete Keyword to Remove Object Properties
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+
+console.log(foods);
+
+// 17. Check if an Object has a Property
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+function isEveryoneHere(userObj) {
+  return (
+    "Alan" in userObj &&
+    "Jeff" in userObj &&
+    "Sarah" in userObj &&
+    "Ryan" in userObj
+  );
+}
+
+console.log(isEveryoneHere(users));
+
+// 18. Iterate Through the Keys of an Object with a for...in Statement
+const users = {
+  Alan: {
+    online: false,
+  },
+  Jeff: {
+    online: true,
+  },
+  Sarah: {
+    online: false,
+  },
+};
+
+function countOnline(usersObj) {
+  let usersNumber = [];
+  for (let user in usersObj) {
+    if (usersObj[user].online === true) {
+      usersNumber.push(1);
+    }
+  }
+  return usersNumber.length;
+}
+
+console.log(countOnline(users));
+
+// 19. Generate an Array of All Object Keys with Object.keys()
+let users = {
+  Alan: {
+    age: 27,
+    online: false,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: false,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+function getArrayOfUsers(obj) {
+  // Only change code below this line
+  return Object.keys(obj);
+  // Only change code above this line
+}
+
+console.log(getArrayOfUsers(users));
+
+// 20. Modify an Array Stored in an Object
+let user = {
+  name: "Kenneth",
+  age: 28,
+  data: {
+    username: "kennethCodesAllDay",
+    joinDate: "March 26, 2016",
+    organization: "freeCodeCamp",
+    friends: ["Sam", "Kira", "Tomo"],
+    location: {
+      city: "San Francisco",
+      state: "CA",
+      country: "USA",
+    },
+  },
+};
+
+function addFriend(userObj, friend) {
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+}
+
+console.log(addFriend(user, "Pete"));
